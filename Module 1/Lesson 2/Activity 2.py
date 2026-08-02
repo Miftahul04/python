e5a739ae5efd0107c35cc3cfe392c18e1e003a6e
Miftahul04 <ranisomc37@gmail.com>
2026-08-02 20:07:09 +0600

    lesson 2

---
+++ Module 1/Lesson 2/Activity 2.py
@@ -0,0 +1,10 @@
+age= 30
+print(age)
+x= "Miftahul" 
+print(x)
+Is_student= "True"
+print(Is_student)
+#Taking input from user
+name= input("What is your name?")
+print(f"Hello, {name},Welcome to Python.")
+            

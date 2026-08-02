872eb800d6274a271aad97eb6a5b13198e22a526
Miftahul04 <ranisomc37@gmail.com>
2026-08-02 19:39:44 +0600

    1st lesson

---
+++ Module 1/Lesson 1/Activity.py
@@ -0,0 +1,5 @@
+print("Welcome to python program")
+#This is a comment
+print("Hello my name is Miftahul, Today is my first class.")
+#\n->new line
+print("I am very excited for this class.\nLooking forward to this opportunity.")
---
+++ Module 1/Lesson 1/Homework.py
@@ -0,0 +1,11 @@
+print("A Day in my Life") 
+#Step 1 
+print("I wake up at 7:00 AM.\n I freshen up and get dressed.") 
+#step 2
+print("I have breakfast.\n Then I leave for school.")
+#step 3 
+print("School ends at 1:45 PM.\n Then I come home.\n I take shower and have lunch.\n Then I take a nap.")
+#Step-4
+print("In the afternoon, I workout and have some food.\n Then I start studying until 10 PM.")
+#step 5
+print("Then I have supper and go to sleep.")

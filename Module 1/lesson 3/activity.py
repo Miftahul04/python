4f009ccacda60ec8d3c81c2b07285397f990c4a5
Miftahul04 <ranisomc37@gmail.com>
2026-08-08 16:36:49 +0600

    lesson 3

---
+++ Module 1/lesson 3/activity.py
@@ -0,0 +1,10 @@
+x = 45
+print(type(x))
+y = 23.45
+print(type(y))
+z = "Python"
+print(type(z))
+isStudent = True
+print(type(isStudent))
+name = input("What is your name?")
+print(f"Hello {name} welcome to codinzy")

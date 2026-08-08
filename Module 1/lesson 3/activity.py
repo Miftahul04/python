8db2f7dc5d813aa9d68ef949625265b6078a7f8a
Miftahul04 <ranisomc37@gmail.com>
2026-08-08 16:55:48 +0600

    lesson 3

--- Module 1/lesson 3/activity.py
+++ Module 1/lesson 3/activity.py
@@ -8,3 +8,13 @@ isStudent = True
 print(type(isStudent))
 name = input("What is your name?")
 print(f"Hello {name} welcome to codinzy")
+x = str(x)
+print(x,type(x))
+y = int(y)
+print(y,type(y))
+#string operation
+print(len(z)) #length
+print(z[::-1]) #reverse
+print(z[3:6]) #slicing
+print(z.upper()) #uppercase
+print(z.lower()) #lowercase

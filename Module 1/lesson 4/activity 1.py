9f8d183e8dac15c91ae8c2443b859f1e47981db9
Miftahul04 <ranisomc37@gmail.com>
2026-08-16 20:25:35 +0600

    lesson 4

---
+++ Module 1/lesson 4/activity 1.py
@@ -0,0 +1,25 @@
+item_price = 25
+quantity = 4
+total = item_price*quantity
+print("Item Price",item_price)
+print("Quantity",quantity)
+print("Total",total)
+#Challenge 1 Add more items
+burger = 120
+pizza = 350
+drink = 80
+total = burger + pizza + drink
+print("Burger price",burger)
+print("Pizza price",pizza)
+print("Drink price", drink)
+print("Total Bill",total)
+
+#Challenge 2 % & //
+remainder = 17%5
+print("Remainder of 17 divied by 5",remainder)
+division = 17/5
+print("Division of 17 divided by 5",division)
+floor_division = 17//5
+print("Floor division of 17 divided by 5",floor_division)
+
+    

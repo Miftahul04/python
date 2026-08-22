8c44057e8b88c5fcc54c68791f527999f7a79386
Miftahul04 <ranisomc37@gmail.com>
2026-08-22 16:31:25 +0600

    activity 2

---
+++ Module 1/lesson 5/activity 2.py
@@ -0,0 +1,7 @@
+buying_price = 100
+selling_price = 120
+if selling_price > buying_price:
+    profit = selling_price-buying_price 
+    print(f"Total profit = {profit} BDT")
+else:
+    print("ITs a loss")

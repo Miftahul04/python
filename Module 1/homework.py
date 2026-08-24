b4aacf49eaf0c4e6c4ace9aa1d0990e2d3d8694d
Miftahul04 <ranisomc37@gmail.com>
2026-08-24 16:55:54 +0600

    homework

---
+++ Module 1/homework.py
@@ -0,0 +1,4 @@
+temperature = int(input(Enter the temperature))
+if temperature > 25 
+    print("The Temperature is hot. Wear light clothes")
+    else print("The temperature is cold. Wear warm clothes.")
